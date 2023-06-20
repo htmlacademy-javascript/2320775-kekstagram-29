ba08b3db42f759cf8549e916124a7e4d595d53f1
AnnaVi <annaseer@mail.ru>
2023-06-21 01:50:29 +0300

    Added  functions for all objects

--- js/main.js
+++ js/main.js
@@ -1,19 +1,26 @@
 /*
-напишите необходимые функции для создания массива из 25 сгенерированных объектов.
+Создать массив из 25 сгенерированных объектов.
 Каждый объект массива — описание фотографии, опубликованной пользователем.
 
-Структура каждого объекта должна быть следующей:
+Структура каждого объекта:
 - id, число — идентификатор опубликованной фотографии. Это число от 1 до 25. Идентификаторы не должны повторяться.
 - url, строка — адрес картинки вида photos/{{i}}.jpg, где {{i}} — это число от 1 до 25. Адреса картинок не должны повторяться.
 - description, строка — описание фотографии. Описание придумайте самостоятельно.
 - likes, число — количество лайков, поставленных фотографии. Случайное число от 15 до 200.
 - comments, массив объектов — список комментариев, оставленных другими пользователями к этой фотографии.
 Количество комментариев к каждой фотографии — случайное число от 0 до 30. Все комментарии генерируются случайным образом.
+Поле avatar — это строка, значение которой формируется по правилу img/avatar-{{случайное число от 1 до 6}}.svg.
 */
+
+const LIKES_MIN = 15;
+const LIKES_MAX = 200;
+const PICTURE_NUMBER = 25;
+const AVATAR_NUMBER = 6;
+const COMMENT_NUMBER = 30;
 
 const NAMES = ['Облачко', 'Рыжуля', 'Тигра', 'Дымок', 'Барсик', 'Уголёк', 'Черныш', 'Батон', 'Булочка', 'Персик'];
 
-const MESSAGES = [
+const COMMENTS = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -23,6 +30,71 @@ const MESSAGES = [
 ];
 
 const DESCRIPTIONS = [
-
-
+  'Вот как выглядит пустой пляж зимой - солнце, лежаки и никого',
+  'Деревянный указатель "На пляж", выполнен, похоже, в пиратском стиле.',
+  'Голубая вода, голубое небо, всегда тепло и ясно - что ещё надо?',
+  'Какие красивые глаза у неё...',
+  'А бармен - шутник, однако!',
+  'Кто-то съел мой завтрак.',
+  'Юпи вернулся!',
+  'Заберите нас отсюда!',
+  'Интересная конструкция. Надо купить домой такую.',
+  'И здесь одни заборы?((',
 ];
+
+//Функция для генерации случайного числа.
+const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+//Функция для генерации случайного элемента массива.
+const getRandomArrayElement = (elements) =>
+  elements[getRandomInteger(0, elements.length - 1)];
+
+//Функция для генерации id
+const createIdGenerator = () => {
+  let lastGeneratedId = 0;
+
+  return () => {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
+  };
+};
+
+const generateCommentId = createIdGenerator();
+const generatePictureId = createIdGenerator();
+
+//Функция для генерации сообщения.
+//Создаётся массив длиной из 1-2 случайных элементов массива comments
+//и склеивается методом join
+const createMessage = () => Array.from(
+  { length: getRandomInteger(1, 2) },
+  () => getRandomArrayElement(COMMENTS),
+).join(' ');
+
+const createComment = () => ({
+  id: generateCommentId(),
+  avatar: `img/avatar-${getRandomInteger(1, AVATAR_NUMBER)}.svg`,
+  message: createMessage(),
+  name: getRandomArrayElement(NAMES),
+});
+
+//Функция для генерации объекта-фотографии.
+const createPicture = () => {
+  const id = generatePictureId();
+  return {
+    id: id,
+    url: `photos/${id}.jpg`,
+    description: getRandomArrayElement(DESCRIPTIONS),
+    likes: getRandomInteger(LIKES_MIN, LIKES_MAX),
+    comments: Array.from(
+      { length: getRandomInteger(0, COMMENT_NUMBER) },
+      createComment,
+    ),
+  };
+};
+
+const getPictures = () => Array.from(
+  { length: PICTURE_NUMBER },
+  createPicture,
+);
+
+getPictures();
