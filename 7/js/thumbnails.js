ed922297b7bb858880f111df5b3289b991ce037a
Keks <keks@htmlacademy.ru>
2023-07-02 14:14:57 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/thumbnails.js
+++ 7/js/thumbnails.js
@@ -1,14 +1,14 @@
-const picturesContainer = document.querySelector('.pictures'); //Найти тег с классом в разметке (DOM-element)
-const templateFragment = document.querySelector('#picture').content.querySelector('.picture'); //Найти шаблон по id и через content его содержимое
+const picturesContainer = document.querySelector('.pictures'); //Находит тег с классом в разметке (DOM-element)
+const templateFragment = document.querySelector('#picture').content.querySelector('.picture'); //Находит шаблон по id и через content его содержимое
 
 //1. Создание DOM-элемента
 //2. Наполнение шаблона данными
-const makeOneThumbnail = ({ comments, description, likes, url }) => { //деструктуризация массива для 4 переменных
+const makeOneThumbnail = ({ comments, description, likes, url }) => { //Метод деструктуризации массива для 4х переменных
 
-  const thumbnail = templateFragment.cloneNode(true); //Клонирование содержимого шаблона
-  thumbnail.querySelector('.picture__img').src = url; //Использование точечной нотации для обращения к свойству
+  const thumbnail = templateFragment.cloneNode(true); //Клонирует содержимого шаблона
+  thumbnail.querySelector('.picture__img').src = url; //Использует точечную нотацию для обращения к свойству
   thumbnail.querySelector('.picture__img').alt = description;
-  thumbnail.querySelector('.picture__comments').textContent = comments.length; //Указать количество комментариев
+  thumbnail.querySelector('.picture__comments').textContent = comments.length; //Указывает количество комментариев
   thumbnail.querySelector('.picture__likes').textContent = likes;
 
   return thumbnail;
@@ -16,13 +16,14 @@ const makeOneThumbnail = ({ comments, description, likes, url }) => { //дест
 
 //3. Функция для отрисовки созданных элементов
 const getAllThumbnails = (pictures) => { //Принмает массив pictures
-  const fragment = document.createDocumentFragment(); //Создание временного хранилища для эолементов
-  pictures.forEach((picture) => { //Цикл перебора данных массива
-    const thumbnail = makeOneThumbnail(picture); //Создание одного DOM-элемента
-    fragment.append(thumbnail);
-  });
-
-  picturesContainer.append(fragment); //Добавляет созданный массив в DOM-дерево
+  const fragment = document.createDocumentFragment(); //Создаёт временное хранилище для элементов
+  for (let i = 0; i < pictures.length; i++) {
+    const picture = pictures[i];
+    const thumbnail = makeOneThumbnail(picture); //Создаёт одноин DOM-элемент
+    fragment.append(thumbnail); //Добавляет элемент в переменную хранилища
+    picturesContainer.append(fragment); //Добавляет созданный массив в DOM-дерево
+  }
+  return fragment;
 };
 
 export {getAllThumbnails};
