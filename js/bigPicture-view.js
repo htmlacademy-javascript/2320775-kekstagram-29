6ba7d2a9851824e9caf4ba63cf9b172ed5a8b1b5
AnnaVi <annaseer@mail.ru>
2023-07-05 14:47:17 +0300

    Made new module 'bigPicture-view'

---
+++ js/bigPicture-view.js
@@ -0,0 +1,23 @@
+const bigPicture = document.querySelector('.big-picture');
+
+
+
+
+// <!-- Полноэкранный показ изображения -->
+// <section class="big-picture  overlay  hidden">
+//   <h2 class="big-picture__title  visually-hidden">Просмотр фотографии</h2>
+//   <div class="big-picture__preview">
+
+//     <!-- Просмотр изображения -->
+//     <div class="big-picture__img">
+//       <img src="img/logo-background-3.jpg" alt="Девушка в купальнике" width="600" height="600">
+//     </div>
+
+/* <li class="social__comment">
+    <img
+        class="social__picture"
+        src="{{аватар}}"
+        alt="{{имя комментатора}}"
+        width="35" height="35">
+    <p class="social__text">{{текст комментария}}</p>
+</li> */
