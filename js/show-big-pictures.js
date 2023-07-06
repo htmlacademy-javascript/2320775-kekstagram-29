d3f53a0684ca3b60328ddb9f3c0d5c3a964632d6
AnnaVi <annaseer@mail.ru>
2023-07-06 17:21:15 +0300

    Fixed social picures avatars and text when the big picture is open, also added emptu line inner utml for UL (comments list)

--- js/show-big-pictures.js
+++ js/show-big-pictures.js
@@ -6,10 +6,11 @@ const closeButton = bigPicture.querySelector('.big-picture__cancel'); //<button 
 const bigPictureImage = bigPicture.querySelector('.big-picture__img img'); //<div class="big-picture__img">
 const bigPictureSocial = bigPicture.querySelector('.big-picture__social'); //<div class="big-picture__social  social">
 const likesCount = bigPicture.querySelector('.likes-count'); //<class="social__likes">Нравится <span class="likes-count">356</span>
-const socialCommentsCount = bigPicture.querySelector('.comments-count'); //<div class="social__comment-count">5 из <span class="comments-count">125</span>комментариев</div>
+const socialCommentsCount = bigPicture.querySelector('.social__comment-count'); //<div class="social__comment-count">5 из <span class="comments-count">125</span>комментариев</div>
 const socialCommentsList = bigPicture.querySelector('.social__comments'); //<ul class="social__comments">
 const socialCommentsItem = bigPicture.querySelector('.social__comment'); //<li class="social__comment">
 const socialCommentsLoader = bigPicture.querySelector('.comments-loader'); //<button type="button" class="social__comments-loader comments-loader">Загрузить еще</button>
+const socialCommentCaption = bigPicture.querySelector('.social__caption'); //<p class="social__caption">Тестим новую камеру! =)</p>
 
 //1.
 const openBigPicture = () => {
@@ -41,15 +42,16 @@ const fillBigPictureImage = (data) => {
   bigPictureImage.src = data.url;
   bigPictureImage.alt = data.description;
   likesCount.textContent = data.likes;
+  socialCommentCaption.textContent = data.description;
 };
 
 //2.
 
 const fillCommentElement = (element) => {
   const comment = socialCommentsItem.cloneNode(true);
-  const bigPictureSocialAvatar = comment.querySelector('.social__picture');//<img class="social__picture" src="img/avatar-1.svg" alt="Аватар автора фотографии">
-  bigPictureSocial.src = element.avatar;
-  bigPictureSocial.alt = element.name;
+  const socialPictureAvatar = comment.querySelector('.social__picture');//<img class="social__picture" src="img/avatar-1.svg" alt="Аватар автора фотографии">
+  socialPictureAvatar.src = element.avatar;
+  socialPictureAvatar.alt = element.name;
   comment.querySelector('.social__text').textContent = element.message; //<p class="social__text">Мега фото!</p>
   return comment;
 };
@@ -66,6 +68,7 @@ const hideTemporaryElements = () => {
 
 //Вывод
 const showBigPictures = (data) => {
+  socialCommentsList.innerHTML = '';
   openBigPicture();
   fillBigPictureImage(data);
   fillCommentsList(data.comments);
